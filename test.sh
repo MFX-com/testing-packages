05e02586a088661cbf1ceade0d67a6ac69d933a0
Marko <marko.smej@outlook.com>
2023-06-22 18:41:46 +0200

    test

---
+++ test.sh
@@ -0,0 +1,5 @@
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer 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" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/MFX-com/testing-packages/pulls/PULL_NUMBER/reviews

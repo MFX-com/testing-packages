6139056420e3a51b39da1ba2e297303f1f84ae44
Marko <marko.smej@outlook.com>
2023-08-07 17:25:38 +0200

    wip 4

--- ethers.js
+++ ethers.js
@@ -1,6 +1,7 @@
 const { ethers } = require("ethers");
 
 
+
 const wallet = ethers.Wallet.createRandom();
 console.log(`Private Key: ${wallet.privateKey}`);
 console.log(`Address: ${wallet.address}`);

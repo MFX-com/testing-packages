f65206b7ea37048509c42891406ffc9c1b76db8d
Marko <marko.smej@outlook.com>
2023-08-28 17:42:20 +0200

    wip

--- testing.bash
+++ testing.bash
@@ -12,36 +12,41 @@
 #
 #find deploy/*.json -type f -exec sed -i -e "s/\${DS_LOKI}/$loki_ds/g" {} +
 
-response_code=$(curl -s -o /dev/null -w "%{http_code}" https://flask-news.marefx.com/test-status)
-response_code_api=$(curl -s -o /dev/null -w "%{http_code}" https://api-dev-k8s.nexera.id)
-response_code_cms=$(curl -s -o /dev/null -w "%{http_code}" https://cms-dev.nexera.id/healthcheck)
-response_code_data_storage=$(curl -s -o /dev/null -w "%{http_code}" https://data-storage.dev.nexera.id/api/healthcheck)
+#response_code=$(curl -s -o /dev/null -w "%{http_code}" https://flask-news.marefx.com/test-status)
+#response_code_api=$(curl -s -o /dev/null -w "%{http_code}" https://api-dev-k8s.nexera.id)
+#response_code_cms=$(curl -s -o /dev/null -w "%{http_code}" https://cms-dev.nexera.id/healthcheck)
+#response_code_data_storage=$(curl -s -o /dev/null -w "%{http_code}" https://data-storage.dev.nexera.id/api/healthcheck)
+#
+#
+#printf "Response code: %d\n" "$response_code"
+#printf "Response code: %d\n" "$response_code_api"
+#printf "Response code: %d\n" "$response_code_cms"
+#printf "Response code: %d\n" "$response_code_data_storage"
+#
+#my_timer=$(date +%s)
+#
+#printf "Date: %s\n" "$my_timer"
+#
+#testing="hellO"
+#echo "${testing^^}"
+#
+#app_url="https://flask-news.marefx.com/test-status"
+#attempts=0
+#total_attempts=5
+#while [ $attempts -lt $total_attempts ]; do
+#  status_code=$(curl -s -o /dev/null -w '%{http_code}' $app_url)
+#  printf "The response code is: %d\n" "$status_code"
+#
+#  if [ "$status_code" -eq 200 ]; then
+#    attempts=$((attempts+1))
+#    sleep 3
+#  else
+#    printf "The application has issues starting up, check the logs!"
+#    exit 1
+#  fi
+#done
 
 
-printf "Response code: %d\n" "$response_code"
-printf "Response code: %d\n" "$response_code_api"
-printf "Response code: %d\n" "$response_code_cms"
-printf "Response code: %d\n" "$response_code_data_storage"
+changes="$(git log --pretty=oneline 71bcdec22ff0c63384e2a36a5be72a6cf8faf4cc -n 1 | cat -)"
 
-my_timer=$(date +%s)
-
-printf "Date: %s\n" "$my_timer"
-
-testing="hellO"
-echo "${testing^^}"
-
-app_url="https://flask-news.marefx.com/test-status"
-attempts=0
-total_attempts=5
-while [ $attempts -lt $total_attempts ]; do
-  status_code=$(curl -s -o /dev/null -w '%{http_code}' $app_url)
-  printf "The response code is: %d\n" "$status_code"
-
-  if [ "$status_code" -eq 200 ]; then
-    attempts=$((attempts+1))
-    sleep 3
-  else
-    printf "The application has issues starting up, check the logs!"
-    exit 1
-  fi
-done
+echo "$changes"
